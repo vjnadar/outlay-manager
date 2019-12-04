58f305e7cb575879f5c2d081625b4a297d522a88
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2019-12-04 11:49:11 +0530

    removed redundant lines

--- frontend/src/components/UI/Spinner/Spinner.js
+++ frontend/src/components/UI/Spinner/Spinner.js
@@ -2,24 +2,7 @@ import React from "react";
 
 import "./Spinner.scss";
 const Spinner = props => {
-  // const [timeOut, setTimer] = useState(false);
   let spinner = <div className="loader"></div>;
-  // if (timeOut) {
-  //   spinner = (
-  //     <Row>
-  //       <Col>
-  //         <h4>Sorry! Something went wrong. Please try again later.</h4>
-  //       </Col>
-  //     </Row>
-  //   );
-  // }
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     console.log("Done");
-  //     setTimer(true);
-  //   }, 10000);
-  // }, []);
-
   return <> {spinner}</>;
 };
 
