45e29af2e8920df70c03361b7d4923d1f9a815d5
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2019-12-09 18:05:41 +0530

    minor mod

--- frontend/src/http/axios.js
+++ frontend/src/http/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axios_main = axios.create({
-  baseURL: "http://49.205.139.196:8080/",
+  baseURL: "http://localhost:8080/",
   headers: {
     "Content-Type": "application/json"
   }
