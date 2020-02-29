88ba8818b634ac7ec7baf296fa76ffb21a1be272
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2020-02-29 13:25:41 +0530

    removed CWM

--- frontend/src/hoc/SetTokenHeader/SetTokenHeader.js
+++ frontend/src/hoc/SetTokenHeader/SetTokenHeader.js
@@ -3,23 +3,28 @@ import { AppContext } from "../../contexts/contexts";
 
 const SetTokenHeader = (WrappedComponent, axios) => {
   return class extends Component {
-    static contextType = AppContext;
-
-    componentWillMount() {
-      this.interceptor = axios.interceptors.request.use(
-        config => {
-          config.headers.Authorization = `Bearer ${this.context.token}`;
-          return config;
-        }
-        // error=> {
-        //
-        //   return Promise.reject(error);
-        // }
-      );
+    constructor(props) {
+      super(props);
+      this.state = {
+        interceptor: axios.interceptors.request.use(
+          config => {
+            console.log("Mount");
+            config.headers.Authorization = `Bearer ${this.context.token}`;
+            console.log(config);
+            return config;
+          }
+          // error=> {
+          //
+          //   return Promise.reject(error);
+          // }
+        )
+      };
     }
 
+    static contextType = AppContext;
+
     componentWillUnmount() {
-      axios.interceptors.request.eject(this.interceptor);
+      axios.interceptors.request.eject(this.state.interceptor);
     }
 
     render() {
