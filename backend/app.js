33aa5f8a94a7e311c2c5b47afcc3bbfd9e88e3d6
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2020-03-09 18:10:21 +0530

    added the helmet module.

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const statsRoutes = require("./routes/statsRoutes/statsRoutes");
 const authenticationRoutes = require("./routes/authenticationRoutes/authenticationRoutes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet=require("helmet");
 const connectToMongoDb = require("./databaseConnection/connectToDb")
   .connectToDb;
 const getDbAccess = require("./databaseConnection/connectToDb").getDbAccess;
 require("dotenv").config();
 
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
