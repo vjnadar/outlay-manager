8e1f10c91910f57cb3990c2ed4b6e671857b5ec6
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2020-07-11 11:49:23 +0530

    removed console.log

--- frontend/src/components/DatePicker/DateRangePicker.js
+++ frontend/src/components/DatePicker/DateRangePicker.js
@@ -2,7 +2,6 @@ import React from "react";
 import { DateRangePicker as ReactDateRangePicker } from "react-dates";
 
 const DateRangePicker = props => {
-  console.log(props)
   return (
     <>
       <ReactDateRangePicker
